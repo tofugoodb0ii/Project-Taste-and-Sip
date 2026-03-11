9c8a506f1dcdb683ba05db5c65d444454ede4b69
tofugoodb0ii <thattep3014@gmail.com>
2026-03-11 23:19:17 +0700

    Delete week 2 directory

--- week 2/2_membership_operators.py
+++
@@ -1,7 +0,0 @@
-#membership operators
-
-x = ["ComED" , "KKU"]
-#print("KKU" in x)
-
-y = ["tomato" , "potato" , "onion" , "paper" , "garlic"]
-print("beef" not in y)
--- week 2/4_days.py
+++
@@ -1,6 +0,0 @@
-#test operators
-
-x = int(input("input number of day : "))
-print(x ,"days" , "is" , x*24 , "hour" )
-print(x ,"days" , "is" , x*24*60 , "minutes" )
-print(x ,"days" , "is" , x*24*60*60 , "second" )
